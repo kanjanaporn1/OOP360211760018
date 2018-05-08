f31ef6e227a6d4f385ad8232e58d6fe6970799bb
kanjanaporn <ray.kanjanaporn1@gmail.com>
2018-05-08 15:13:51 +0700

    update data

--- src/oopmit/Hello.java
+++ src/oopmit/Hello.java
@@ -3,5 +3,9 @@ package oopmit;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, my name is kanjanaporn");
+        System.out.println("Major: Information System");
+        System.out.println ("Faculty: Management Technology");
+        System.out.println("University: RMUTSV");
+
     }
 }
