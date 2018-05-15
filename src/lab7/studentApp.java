f92622cd3e731b2c311c233f4a8f63418d0f6e57
kanjanaporn <ray.kanjanaporn1@gmail.com>
2018-05-15 14:21:18 +0700

    input data from user

--- src/lab7/studentApp.java
+++ src/lab7/studentApp.java
@@ -1,8 +1,13 @@
 package lab7;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class studentApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Student s1 =new Student();
         s1.setName("Mit");
         s1.setAge(22);
@@ -13,7 +18,41 @@ public class studentApp {
         Student s2 =new Student("Safe",90,"Female",190);
         System.out.println(s2.toString());
 
+        //input data from user
+        Student s3 = new Student();
+        s3 = inputData (s3); //s
+        s3 = inputDataBuffer(s3);
+        System.out.println(s3.toString());
 
+    }//main
+
+    private static  Student inputDataBuffer (Student s) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please enter your information: ");
+        System.out.println("Name: ");
+        s.setName(reader.readLine());
+        System.out.println(":Age: ");
+        s.setAge(Integer.parseInt(reader.readLine()));
+        System.out.println("Gender: ");
+        s.setHeight(Double.parseDouble(reader.readLine())) ;
+
+        return  s;
 
     }
+
+    private static Student inputData(Student s3) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please rnter your information");
+        System.out.println("Name: ");
+        s3.setName(sc.nextLine());
+        System.out.println("Age");
+        s3.setAge(sc.nextInt());
+        System.out.println("Gender:");
+        s3.setGrnder(sc.nextLine());
+        System.out.println("Heih:");
+        s3.setHeight(sc.nextDouble());
+
+        return s3;
+    }
+
 }
